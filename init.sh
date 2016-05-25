0dd59ce1c97f3bae539ffb52da13f57f27ff0f88
ExSoviet <exsoviet@mail.ru>
2016-05-25 17:56:07 +0300

    7 ok

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo ln -sf /home/box/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 127.0.0.1:8080 --workers=1 hello:app -D
+sudo gunicorn -b 0.0.0.0:8080 --workers=1 -c hello.py hello:app -D
